6615312b89be3de31b5f4c8c2fd6cb68bbf38795
Chuck Bass <chuck@acrocad.net>
2024-09-03 06:36:13 -0700

    This has the first pyproject.toml file for using with poetry.  AI likes this file but I have yet to connecti it up to my pypi account to do a pip install...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="splint",
     version="0.1.0",
-    packages=find_packages(where="src", include=["splint.*"]),
+    packages=find_packages(where="src/splint", include=["splint.*"]),
     # look in 'src' and include packages in 'splint' and its sub-packages
     package_dir={"": "src"},
 )
